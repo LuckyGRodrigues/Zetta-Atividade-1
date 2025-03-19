7c6222c00efbe7ed5bf61e3358de37ffd9a62288
Lucas <lucas_rodriguesguerra@unochapeco.edu.br>
2025-03-19 14:47:22 -0300

    feat/assunto 1 concluido

---
+++ components/diasMes.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-app>
+    <v-container>
+      <h2 class="align-center mt-15">Total de Dias: {{ totalDias }}</h2>
+
+      <v-row>
+        <v-col 
+          v-for="ano in anosSelecionados" 
+          :key="ano" 
+          cols="12"
+          md="2"
+          class="ml-11 mt-10"
+        >
+          <h3>{{ ano }}</h3>
+          <v-table height="400">
+            <thead>
+              <tr>
+                <th>Mês</th>
+                <th>Dias</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="mes in mesesExibidos(ano)" :key="mes.nome">
+                <td>{{ mes.nome }}</td>
+                <td>{{ mes.dias }}</td>
+              </tr>
+            </tbody>
+          </v-table>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    anosSelecionados: {
+      type: Array,
+      required: true,
+    },
+    mesesSelecionados: {
+      type: Array,
+      required: true,
+    }
+  },
+
+  computed: {
+    totalDias() {
+      return this.anosSelecionados.reduce((total, ano) => {
+        return total + this.mesesExibidos(ano).reduce((soma, mes) => soma + mes.dias, 0);
+      }, 0);
+    },
+  },
+
+  methods: {
+    diasDoMes(ano) {
+      return [
+        { nome: "Janeiro", dias: 31 },
+        { nome: "Fevereiro", dias: this.anoBissexto(ano) ? 29 : 28 },
+        { nome: "Março", dias: 31 },
+        { nome: "Abril", dias: 30 },
+        { nome: "Maio", dias: 31 },
+        { nome: "Junho", dias: 30 },
+        { nome: "Julho", dias: 31 },
+        { nome: "Agosto", dias: 31 },
+        { nome: "Setembro", dias: 30 },
+        { nome: "Outubro", dias: 31 },
+        { nome: "Novembro", dias: 30 },
+        { nome: "Dezembro", dias: 31 },
+      ];
+    },
+
+    anoBissexto(ano) {
+      return (ano % 4 === 0 && ano % 100 !== 0) || ano % 400 === 0;
+    },
+
+    mesesExibidos(ano) {
+      const todosOsMeses = this.diasDoMes(ano);
+      if (this.mesesSelecionados.length === 0) {
+        return todosOsMeses; // Se não há filtro, exibe todos
+      }
+      return todosOsMeses.filter(mes => this.mesesSelecionados.includes(mes.nome));
+    }
+  },
+};
+</script>
+
+<style>
+.align-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,139 +1,67 @@
 <template>
-  <template>
-    <v-card style="background-color:  #fb911f;">
-      <v-tabs v-model="tab" align-tabs="center" color="red">
-        <a href="http://localhost:3000/ademiro/ademiro">
-          <v-icon class="bah">
-            mdi-account-lock
-          </v-icon>
-        </a>
-
-        <a href="http://localhost:3000/">
-          <v-tab :value="1" @click="" style="color: darkred; font-weight: bolder; 
-          font-size:larger;" class="text-none">Início</v-tab>
-        </a>  
-
-        <a href="http://localhost:3000/cupom">
-          <v-tab :value="2" @click="" style="color: darkred; font-weight: bold; 
-          font-size:large;" class="text-none">Cupons</v-tab>
-        </a>
-
-        <a href="http://localhost:3000/cardapio">
-          <v-tab :value="3" @click="" style="color: darkred; font-weight: bold;
-          font-size:large;" class="text-none">Cardápio</v-tab>
-        </a>
-
-        <a href="https://www.youtube.com/watch?v=hvL1339luv0">
-          <v-tab :value="4" @click="" style="color: darkred; font-weight: bold;
-          font-size:large;" class="text-none">AppBK</v-tab>
-        </a>
-
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
-          <v-tab :value="5" @click="" style="color: darkred; font-weight: bold;
-          font-size:large;" class="text-none">ClubeBK</v-tab>
-        </a>
+  <v-app>
+    <v-sheet class="mx-auto mt-15" width="400">
+      <v-form @submit.prevent="updateAnosSelecionados">
+        <v-text-field
+          v-model="anoFinal"
+          :rules="anoRules"
+          label="Ano Final"
+          type="number"
+        ></v-text-field>
         
-        <a href="http://localhost:3000/login">
-          <v-icon class="bah" style="position: relative; width: 50%;">
-            mdi-login
-          </v-icon>
-        </a>
+        <v-select
+          v-model="mesesSelecionados"
+          :items="mesesDisponiveis"
+          label="Selecione os meses"
+          multiple
+          chips
+        ></v-select>
 
-      </v-tabs>
-    </v-card>
-  </template>
+        <v-btn class="mt-2" type="submit" block>Atualizar</v-btn>
+      </v-form>
+    </v-sheet>
 
-  <body>
-    <section>
-      <div class="background-section">
-        <video src="../Video/a.mp4" autoplay loop muted></video>
-      </div>
-      <div class="box">
-        <div class="conteudo">
-          <pre>
-            O AMOR PODE ACABAR
-            O BOM GOSTO NÃO!
-          </pre>
-          <img src="/imagem/burgasso.png" alt="" style="height: 75%; width: 75%;">
-        </div>
-      </div>
-    </section>
-  </body>
+    <dias-mes v-if="mostrarResultado" :anosSelecionados="anosSelecionados" :mesesSelecionados="mesesSelecionados"></dias-mes>
+  </v-app>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      tab: null,
-    }),
-  }
+import DiasMes from "../components/diasMes.vue";
+
+export default {
+  components: {
+    DiasMes,
+  },
+  data() {
+    return {
+      anoFinal: new Date().getFullYear(),
+      anosSelecionados: this.generateAnosSelecionados(new Date().getFullYear()),
+      mesesSelecionados: [],
+      mesesDisponiveis: [
+        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+      ],
+      anoRules: [
+        (v) => !!v || "Required",
+        (v) => v >= new Date().getFullYear() || "Ano deve ser maior ou igual ao ano atual"
+      ],
+      mostrarResultado: false,
+    };
+  },
+  methods: {
+    generateAnosSelecionados(anoFinal) {
+      const currentYear = new Date().getFullYear();
+      const anos = [];
+      for (let ano = currentYear; ano <= anoFinal; ano++) {
+        anos.push(ano);
+      }
+      return anos;
+    },
+
+    updateAnosSelecionados() {
+      this.anosSelecionados = this.generateAnosSelecionados(this.anoFinal);
+      this.mostrarResultado = true;
+    },
+  },
+};
 </script>
-
-<style>
-  *{
-    padding: 0;
-    margin: 0;
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    box-sizing: border-box;
-  }
-
-  body{
-    width: 100%;
-    min-height: 100vh;
-  }
-  section{
-    width: 100%;
-    height: 100vh;  
-  }
-
-  section div.background-section{
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    z-index: 0;
-  }
-
-  section div.background-section video{
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    object-position: center;
-  }
-
-  section div.box{
-    width: 50%;
-    height: 100%;
-    position: relative;
-    z-index: 1;
-    background: linear-gradient(to right, blue(0,0,0,0.9), transparent);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 2%;
-  }
-
-  section div.box div.conteudo{
-    width: 80%;
-  }
-
-  section div.box img{
-    width: 100%;
-    min-width: 300px;
-  }
-
-  section div.box pre{
-    font-family: 'Times New Roman', Times, serif;
-    font-weight: bolder;
-    color: #b59143;
-    font-size: 24px;
-    max-height: 32px;
-  }
-
-  section div.box v-tab{
-    color: yellow;
-  }
-
-  .bah{
-    color:darkred
-  }
-</style>
